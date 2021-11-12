49c8ce36edb986e9194b8bd370377baef389c31d
Rafael Mendes <dossantosgit81@gmail.com>
2021-11-12 17:47:08 -0300

    Trabalhando com validações com hibernate validator, e repositorios

--- src/main/java/io/github/mendes/socialstudy/rest/UserResource.java
+++ src/main/java/io/github/mendes/socialstudy/rest/UserResource.java
@@ -1,6 +1,11 @@
 package io.github.mendes.socialstudy.rest;
 
+import java.util.Set;
+
+import javax.inject.Inject;
 import javax.transaction.Transactional;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validator;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -13,6 +18,7 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import io.github.mendes.socialstudy.domain.model.User;
+import io.github.mendes.socialstudy.domain.repository.UserRepository;
 import io.github.mendes.socialstudy.rest.dto.CreateUserRequest;
 import io.quarkus.hibernate.orm.panache.PanacheQuery;
 
@@ -21,19 +27,40 @@ import io.quarkus.hibernate.orm.panache.PanacheQuery;
 @Produces(MediaType.APPLICATION_JSON)
 public class UserResource {
 	
+	private UserRepository repository;
+	private Validator validator;
+	
+	
+	@Inject
+	public UserResource(UserRepository repository, Validator validator) {
+		this.repository = repository;
+		this.validator = validator;
+	}
+	
 	@POST
 	@Transactional
 	public Response createUser(CreateUserRequest userRequest) {
+		Set<ConstraintViolation<CreateUserRequest>> violations = validator.validate(userRequest);
+		
+		if(!violations.isEmpty()) {
+			ConstraintViolation<CreateUserRequest> erro = violations.stream().findAny().get();
+			String erroMessage = erro.getMessage();
+			return Response.status(400).entity(erroMessage).build();
+		}
+		
 		User user = new User();
 		user.setAge(userRequest.getAge());
 		user.setName(userRequest.getName());
-		user.persist();
+		
+		repository.persist(user);
+		
 		return Response.ok(user).build();
 	}
 	
 	@GET
 	public Response listAllUsers() {
-		PanacheQuery<User> query = User.findAll(); 
+		PanacheQuery<User> query = repository.findAll();
+		
 		return Response.ok(query.list()).build();
 	}
 	
@@ -41,10 +68,10 @@ public class UserResource {
 	@Path("{id}")
 	@Transactional
 	public Response deleteUser(@PathParam("id") Long id) {
-		User user = User.findById(id);
+		User user = repository.findById(id);
 		
 		if(user != null) {
-			user.delete();
+			repository.delete(user);
 			return Response.ok().build();
 		}
 		
@@ -55,7 +82,7 @@ public class UserResource {
 	@Path("{id}")
 	@Transactional
 	public Response updateUser(@PathParam("id") Long id, CreateUserRequest userData) {
-		User user = User.findById(id);
+		User user = repository.findById(id);
 		
 		if(user != null) {
 			user.setName(userData.getName());
--- src/main/java/io/github/mendes/socialstudy/rest/dto/CreateUserRequest.java
+++ src/main/java/io/github/mendes/socialstudy/rest/dto/CreateUserRequest.java
@@ -1,9 +1,15 @@
 package io.github.mendes.socialstudy.rest.dto;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 public class CreateUserRequest {
 	
+	//Impede strings vazia e string nulas
+	@NotBlank(message = "Nome é requerido")
 	private String name;
 	
+	@NotNull(message="Idade é requerida")
 	private Integer age;
 
 	public String getName() {
