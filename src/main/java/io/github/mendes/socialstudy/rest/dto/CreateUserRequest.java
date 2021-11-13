fb91fa2d5af40ad84bcbbb5262752f3366c44288
Rafael Mendes <dossantosgit81@gmail.com>
2021-11-12 23:28:05 -0300

    Adicionando Lombok ao projeto

--- src/main/java/io/github/mendes/socialstudy/rest/dto/CreateUserRequest.java
+++ src/main/java/io/github/mendes/socialstudy/rest/dto/CreateUserRequest.java
@@ -3,31 +3,17 @@ package io.github.mendes.socialstudy.rest.dto;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
+import lombok.Data;
+
+@Data
 public class CreateUserRequest {
 	
-	//Impede strings vazia e string nulas
+	//Impede strings vazias e string nulas
 	@NotBlank(message = "Nome é requerido")
 	private String name;
 	
 	@NotNull(message="Idade é requerida")
 	private Integer age;
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public Integer getAge() {
-		return age;
-	}
-
-	public void setAge(Integer age) {
-		this.age = age;
-	}
-	
 	
 
 }
