ca30b694d050eb8e8895adcf29846b4bf7ceaaa0
Rafael Mendes <dossantosgit81@gmail.com>
2021-11-13 23:30:25 -0300

    Teste invalido de cadastro concluido

--- src/test/java/io/github/mendes/socialstudy/rest/UserResourceTest.java
+++ src/test/java/io/github/mendes/socialstudy/rest/UserResourceTest.java
@@ -1,10 +1,14 @@
 package io.github.mendes.socialstudy.rest;
 
+import java.util.List;
+import java.util.Map;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import io.github.mendes.socialstudy.rest.dto.CreateUserRequest;
+import io.github.mendes.socialstudy.rest.dto.ResponseError;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
@@ -13,7 +17,7 @@ import io.restassured.http.ContentType;
 public class UserResourceTest {
 	
 	@Test
-	@DisplayName("Cria um usuario com sucesso")
+	@DisplayName("Deve criar um usuario com sucesso")
 	public void createUserTest() {
 		var user = new CreateUserRequest();
 		user.setName("Fulano");
@@ -30,6 +34,31 @@ public class UserResourceTest {
 		
 		Assertions.assertEquals(201, response.statusCode());
 		Assertions.assertNotNull(response.jsonPath().getString("id"));
+	}
+	
+	@Test
+	@DisplayName("Deve retornar error quando json não é valido")
+	public void createUserValidationErrorTest() {
+		var user = new CreateUserRequest();
+		user.setAge(null);
+		user.setName(null);
+		
+		var response = 
+				RestAssured
+					.given()
+						.contentType(ContentType.JSON)
+						.body(user)
+					.when()
+						.post("/users")
+					.then().extract().response();
+	 
+		Assertions.assertEquals(ResponseError.UNPROCESSABLE_ENTITY_STATUS, response.statusCode());
+		Assertions.assertEquals("Validation Errors", response.jsonPath().getString("message"));
+		
+		List<Map<String, String>> errors = response.jsonPath().getList("errors");
+		Assertions.assertNotNull(errors.get(0).get("message"));
+		Assertions.assertNotNull(errors.get(1).get("message"));
+//		Assertions.assertEquals("Nome é requerido", errors.get(0).get("message"));
 	}
 
 }
